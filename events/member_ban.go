e180c1a3f8d20ba3e40e578b8e1cb2ec200b66ff
p5nbTgip0r <cb3f02c5@opayq.com>
2022-08-19 00:30:16 -0700

    feat(event): consolidate member ban into member leave, refactor code, etc

--- events/member_ban.go
+++ events/member_ban.go
@@ -1,72 +1,39 @@
 package events
 
-import (
-	"audit/util"
-	"fmt"
-	"github.com/diamondburned/arikawa/v3/api"
-	"github.com/diamondburned/arikawa/v3/discord"
-	"github.com/diamondburned/arikawa/v3/gateway"
-	"time"
-)
-
 func init() {
-	handler = append(handler, func() {
-		s.PreHandler.AddSyncHandler(func(c *gateway.GuildBanAddEvent) {
-			m, _ := s.MemberStore.Member(c.GuildID, c.User.ID)
-			// todo: we can goroutine after this point, since we've grabbed all we need from the cache
-			_, bannerMember, _ := getAuditActioner(c.GuildID, discord.Snowflake(c.User.ID), api.AuditLogData{ActionType: discord.MemberBanAdd})
-
-			var joined string
-			if m != nil {
-				joined = fmt.Sprintf("<t:%d:R>", m.Joined.Time().Unix())
-			} else {
-				joined = "Could not retrieve"
-			}
-
-			embed := userBaseEmbed(c.User, "", true)
-			embed.Color = 0x992D22
-
-			embed.Description = fmt.Sprintf("**:rotating_light: %s was banned**", c.User.Mention())
-			embed.Fields = append(embed.Fields,
-				discord.EmbedField{
-					Name:  "Joined server",
-					Value: joined,
-				},
-			)
-			if bannerMember != nil {
-				embed.Fields = append(embed.Fields,
-					discord.EmbedField{
-						Name:  "Banned by",
-						Value: util.FullTag(bannerMember.User),
-					},
-				)
-			}
-
-			handleAuditError(s.SendMessage(auditChannel, "", *embed))
-		})
-	})
-}
-
-func getAuditActioner(g discord.GuildID, f discord.Snowflake, data api.AuditLogData) (*discord.UserID, *discord.Member, error) {
-	audit, err := s.AuditLog(g, data)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var actionerID *discord.UserID
-	var actionerMember *discord.Member
-	for _, entry := range audit.Entries {
-		// only consider entries within 30 seconds of now
-		if entry.CreatedAt().Sub(time.Now()).Abs().Seconds() > 30 {
-			continue
-		}
-		if entry.TargetID == f {
-			actionerID = &entry.UserID
-			actionerMember, _ = s.Member(g, entry.UserID)
-			break
-		}
-	}
-
-	return actionerID, actionerMember, nil
+	//handler = append(handler, func() {
+	//	s.PreHandler.AddSyncHandler(func(c *gateway.GuildBanAddEvent) {
+	//		m, _ := s.MemberStore.Member(c.GuildID, c.User.ID)
+	//		// todo: we can goroutine after this point, since we've grabbed all we need from the cache
+	//		_, bannerMember, _ := getAuditActioner(c.GuildID, discord.Snowflake(c.User.ID), api.AuditLogData{ActionType: discord.MemberBanAdd})
+	//
+	//		var joined string
+	//		if m != nil {
+	//			joined = fmt.Sprintf("<t:%d:R>", m.Joined.Time().Unix())
+	//		} else {
+	//			joined = "Could not retrieve"
+	//		}
+	//
+	//		embed := userBaseEmbed(c.User, "", true)
+	//		embed.Color = 0x992D22
+	//
+	//		embed.Description = fmt.Sprintf("**:rotating_light: %s was banned**", c.User.Mention())
+	//		embed.Fields = append(embed.Fields,
+	//			discord.EmbedField{
+	//				Name:  "Joined server",
+	//				Value: joined,
+	//			},
+	//		)
+	//		if bannerMember != nil {
+	//			embed.Fields = append(embed.Fields,
+	//				discord.EmbedField{
+	//					Name:  "Banned by",
+	//					Value: util.FullTag(bannerMember.User),
+	//				},
+	//			)
+	//		}
+	//
+	//		handleAuditError(s.SendMessage(auditChannel, "", *embed))
+	//	})
+	//})
 }
--- events/member_leave.go
+++ events/member_leave.go
@@ -3,6 +3,7 @@ package events
 import (
 	"audit/util"
 	"fmt"
+	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/gateway"
@@ -22,7 +23,14 @@ func init() {
 func handleMemberRemove(c *gateway.GuildMemberRemoveEvent, m *discord.Member) {
 	// sleep for a few seconds to give the audit log a chance to catch up
 	time.Sleep(time.Second * 3)
-	_, bannerMember, _ := getAuditActioner(c.GuildID, discord.Snowflake(c.User.ID), api.AuditLogData{ActionType: discord.MemberKick})
+
+	entry, _, actionerMem, _ := getAuditActioner(
+		c.GuildID,
+		discord.Snowflake(c.User.ID),
+		api.AuditLogData{},
+		// only consider bans and kicks
+		mapset.NewSet(discord.MemberBanAdd, discord.MemberKick),
+	)
 
 	var joined string
 	if m != nil {
@@ -40,14 +48,27 @@ func handleMemberRemove(c *gateway.GuildMemberRemoveEvent, m *discord.Member) {
 			Value: joined,
 		},
 	)
-	if bannerMember != nil {
-		embed.Description = fmt.Sprintf("**:boot: %s was kicked**", c.User.Mention())
-		embed.Fields = append(embed.Fields,
-			discord.EmbedField{
-				Name:  "Kicked by",
-				Value: util.FullTag(bannerMember.User),
-			},
-		)
+	if entry != nil && actionerMem != nil {
+		switch entry.ActionType {
+		case discord.MemberBanAdd:
+			embed.Color = 0x992D22
+			embed.Description = fmt.Sprintf("**:rotating_light: %s was banned**", c.User.Mention())
+			embed.Fields = append(embed.Fields,
+				discord.EmbedField{
+					Name:  "Banned by",
+					Value: util.FullTag(actionerMem.User),
+				},
+			)
+		case discord.MemberKick:
+			embed.Color = 0xA84300
+			embed.Description = fmt.Sprintf("**:boot: %s was kicked**", c.User.Mention())
+			embed.Fields = append(embed.Fields,
+				discord.EmbedField{
+					Name:  "Kicked by",
+					Value: util.FullTag(actionerMem.User),
+				},
+			)
+		}
 	} else {
 		embed.Description = fmt.Sprintf("**:outbox_tray: %s left the server**", c.User.Mention())
 		embed.Fields = append(embed.Fields,
@@ -59,4 +80,36 @@ func handleMemberRemove(c *gateway.GuildMemberRemoveEvent, m *discord.Member) {
 	}
 
 	handleAuditError(s.SendMessage(auditChannel, "", *embed))
+}
+
+func getAuditActioner(g discord.GuildID, f discord.Snowflake, data api.AuditLogData, eventTypes mapset.Set[discord.AuditLogEvent]) (
+	entry *discord.AuditLogEntry,
+	actionerID *discord.UserID,
+	actionerMember *discord.Member,
+	err error,
+) {
+	audit, err := s.AuditLog(g, data)
+
+	if err != nil {
+		return
+	}
+
+	for _, e := range audit.Entries {
+		// check if an event type filter was specified
+		if eventTypes.Cardinality() != 0 && !eventTypes.Contains(e.ActionType) {
+			continue
+		}
+		// only consider entries within 30 seconds of now
+		if e.CreatedAt().Sub(time.Now()).Abs().Seconds() > 30 {
+			continue
+		}
+		if e.TargetID == f {
+			entry = &e
+			actionerID = &e.UserID
+			actionerMember, _ = s.Member(g, e.UserID)
+			break
+		}
+	}
+
+	return
 }
