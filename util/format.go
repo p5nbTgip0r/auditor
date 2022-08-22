a5ef634f36cceba9c80052942c2178acf1a958ed
p5nbTgip0r <cb3f02c5@opayq.com>
2022-08-22 14:39:39 -0700

    feat(util): discord jump link

--- util/format.go
+++ util/format.go
@@ -1,10 +1,36 @@
 package util
 
 import (
+	"errors"
 	"fmt"
 	"github.com/diamondburned/arikawa/v3/discord"
+	"strings"
 )
 
 func FullTag(u discord.User) string {
 	return fmt.Sprintf("%s (`%s` | `%d`)", u.Mention(), u.Tag(), u.ID)
+}
+
+func DiscordJumpLink(
+	guildID discord.GuildID,
+	channelID discord.ChannelID,
+	messageID discord.MessageID,
+) (string, error) {
+	var link strings.Builder
+
+	if guildID == discord.NullGuildID || channelID == discord.NullChannelID {
+		return "", errors.New("cannot create jump link without guild and channel IDs")
+	} else {
+		link.WriteString("https://discord.com/channels/")
+		link.WriteString(guildID.String())
+		link.WriteString("/")
+		link.WriteString(channelID.String())
+		link.WriteString("/")
+	}
+
+	if messageID != discord.NullMessageID {
+		link.WriteString(messageID.String())
+	}
+
+	return link.String(), nil
 }
