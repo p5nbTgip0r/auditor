3e9477b87afe2a8f4d42ad5a4a8964ee12743a15
p5nbTgip0r <cb3f02c5@opayq.com>
2022-08-24 04:30:38 -0700

    feat(event): add role create

---
+++ events/role_create.go
@@ -0,0 +1,50 @@
+package events
+
+import (
+	"audit/util"
+	"audit/util/color"
+	"fmt"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"strings"
+)
+
+func init() {
+	handler = append(handler, func() {
+		s.PreHandler.AddHandler(func(c *gateway.GuildRoleCreateEvent) {
+			if !AuditRoleCreate.check(&c.GuildID, nil) {
+				return
+			}
+
+			e := &discord.Embed{
+				Description: fmt.Sprintf("**:crossed_swords: Role created: %s**", c.Role.Name),
+				Color:       color.Green,
+				Timestamp:   discord.NewTimestamp(c.Role.CreatedAt()),
+				Footer:      &discord.EmbedFooter{Text: fmt.Sprintf("ID: %s", c.Role.ID)},
+			}
+
+			e.Fields = append(e.Fields,
+				discord.EmbedField{
+					Name:  "Color",
+					Value: color.ColorViewerLink(c.Role.Color, c.Role.Color.String()),
+				},
+			)
+
+			if c.Role.Icon != "" {
+				// todo archive icon
+				link := fmt.Sprintf("[__Link__](%s)", c.Role.IconURL())
+				e.Fields = append(e.Fields, discord.EmbedField{Name: "Icon", Value: link})
+			} else if c.Role.UnicodeEmoji != "" {
+				e.Fields = append(e.Fields, discord.EmbedField{Name: "Emoji", Value: c.Role.UnicodeEmoji})
+			}
+			e.Fields = append(e.Fields, discord.EmbedField{Name: "Hoisted", Value: util.YesNoBool(c.Role.Hoist)})
+			e.Fields = append(e.Fields, discord.EmbedField{Name: "Mentionable", Value: util.YesNoBool(c.Role.Mentionable)})
+
+			perms := util.PermissionString(c.Role.Permissions)
+			perms = strings.ReplaceAll(perms, "Administrator", "**Administrator**")
+			e.Fields = append(e.Fields, discord.EmbedField{Name: "Permissions", Value: perms})
+
+			handleAuditError(s.SendEmbeds(auditChannel, *e))
+		})
+	})
+}
