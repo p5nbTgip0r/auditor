7355f53e63df35fa685697974cccea391fdf34e7
p5nbTgip0r <cb3f02c5@opayq.com>
2022-08-28 12:34:30 -0700

    feat(util): introduce embed util
    currently has helper functions for fields, might add more later

---
+++ util/embeds.go
@@ -0,0 +1,56 @@
+package util
+
+import (
+	"fmt"
+	"github.com/diamondburned/arikawa/v3/discord"
+)
+
+// AddField generates a discord.EmbedField from the given name, value, and codeTags parameters, then appends that
+// field to the embed.
+//
+// The value of the discord.EmbedField looks like:
+//
+//	"value"
+//
+// If codeTags is true, the given value will be surrounded in `code tags`:
+//
+//	"`value`"
+//
+// This is mainly for cutting down on the boilerplate code with generating a value string, appending, and assigning
+// to discord.Embed.Fields
+func AddField(embed *discord.Embed, name, value string, codeTags bool) {
+	format := "%s"
+	if codeTags {
+		format = "`%s`"
+	}
+	embed.Fields = append(embed.Fields,
+		discord.EmbedField{
+			Name:  name,
+			Value: fmt.Sprintf(format, value),
+		})
+}
+
+// AddUpdatedField generates a discord.EmbedField from the given name, old + new values, and codeTags parameters, then
+// appends that field to the embed.
+//
+// The value of the discord.EmbedField looks like:
+//
+//	"old -> new"
+//
+// If codeTags is true, the given values will be surrounded in `code tags`:
+//
+//	"`old` -> `new`"
+//
+// This is mainly for cutting down on the boilerplate code with generating a value string, appending, and assigning
+// to discord.Embed.Fields
+func AddUpdatedField(embed *discord.Embed, name, old, new string, codeTags bool) {
+	format := "%s -> %s"
+	if codeTags {
+		format = "`%s` -> `%s`"
+	}
+	embed.Fields = append(embed.Fields,
+		discord.EmbedField{
+			Name:  name,
+			Value: fmt.Sprintf(format, old, new),
+		})
+}
