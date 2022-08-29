24e44e8bf7d9a0642b2f5c526bff66fe708f2c3d
p5nbTgip0r <cb3f02c5@opayq.com>
2022-08-29 15:25:10 -0700

    feat(event): add invite delete

---
+++ events/invite_delete.go
@@ -0,0 +1,29 @@
+package events
+
+import (
+	"audit/util"
+	"audit/util/color"
+	"fmt"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+)
+
+func init() {
+	handler = append(handler, func() {
+		s.AddHandler(func(c *gateway.InviteDeleteEvent) {
+			if !AuditInviteDelete.check(&c.GuildID, nil) {
+				return
+			}
+
+			e := &discord.Embed{
+				Description: "**:wastebasket: An invite has been deleted**",
+				Timestamp:   discord.NowTimestamp(),
+				Color:       color.Red,
+			}
+			util.AddField(e, "Code", fmt.Sprintf("[**%s**](https://discord.gg/%s)", c.Code, c.Code), false)
+			util.AddField(e, "Channel", util.ChannelTag(c.ChannelID), false)
+
+			handleAuditError(s.SendEmbeds(auditChannel, *e))
+		})
+	})
+}
